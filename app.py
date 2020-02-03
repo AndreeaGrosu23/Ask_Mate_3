830f20e24691385639f0efd79e485d2a25b278bd
Andreea Grosu <andreea.grosu87@gmail.com>
2020-02-03 13:34:17 +0200

    Add search function

--- app.py
+++ app.py
@@ -9,5 +9,15 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/list', methods=['GET', 'POST'])
+def all_questions():
+    if request.method == 'GET':
+        questions = data_manager.get_all_questions()
+    elif request.method == 'POST':
+        phrase = request.form['search']
+        questions = data_manager.search_in_questions(phrase)
+    return render_template('list.html', questions=questions)
+
+
 if __name__ == '__main__':
     app.run()
--- data_manager.py
+++ data_manager.py
@@ -3,7 +3,36 @@ import database_common
 @database_common.connection_handler
 def get_all_questions(cursor):
     cursor.execute("""
-                    SELECT * FROM questions;
+                    SELECT * FROM question;
                    """)
     all_questions = cursor.fetchall()
     return all_questions
+
+
+@database_common.connection_handler
+def get_all_answers(cursor):
+    cursor.execute("""
+                    SELECT * FROM answer;
+                   """)
+    all_answers = cursor.fetchall()
+    return all_answers
+
+
+
+@database_common.connection_handler
+def search_in_questions(cursor, phrase):
+
+    query = """
+                    SELECT question.id, question.submission_time, question.view_number, question.vote_number, question.title, question.message, question.image 
+                    FROM question
+                    LEFT JOIN answer
+                    ON question.id = answer.question_id
+                    WHERE question.title LIKE %(phrase)s 
+                    OR question.message LIKE %(phrase)s 
+                    OR answer.message LIKE %(phrase)s;
+                   """
+    cursor.execute(query,
+                   {'phrase': '%'+phrase+'%'})
+    results=cursor.fetchall()
+    return results
+
