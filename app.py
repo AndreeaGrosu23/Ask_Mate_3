58feb9e6708bdd4c7a7337c3585c9fc1c64fa4a6
Andreea Grosu <andreea.grosu87@gmail.com>
2020-02-05 11:09:21 +0200

    Add search function and footer

--- app.py
+++ app.py
@@ -9,14 +9,17 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/list', methods=['GET', 'POST'])
+@app.route('/list')
 def all_questions():
-    if request.method == 'GET':
-        questions = data_manager.get_all_questions()
-    elif request.method == 'POST':
-        phrase = request.form['search']
-        questions = data_manager.search_in_questions(phrase)
+    questions = data_manager.get_all_questions()
     return render_template('list.html', questions=questions)
+
+
+@app.route('/search')
+def search():
+    phrase = request.args.get('q')
+    questions = data_manager.search_in_questions(phrase)
+    return render_template('search.html', questions=questions, phrase=phrase)
 
 
 if __name__ == '__main__':
