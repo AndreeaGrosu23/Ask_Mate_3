3d0f974ebd58a58f21e77a793e2125ac7f41e978
Andreea Grosu <andreea.grosu87@gmail.com>
2020-02-06 10:42:14 +0200

    Add display_5_questions, search_questions, fancy_search, edit_answer functions

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    questions = data_manager.get_five_questions()
+    return render_template('index.html', questions=questions)
 
 
 @app.route('/list')
+@app.route('/answer')
 def all_questions():
     questions = data_manager.get_all_questions()
     return render_template('list.html', questions=questions)
@@ -20,6 +22,25 @@ def search():
     phrase = request.args.get('q')
     questions = data_manager.search_in_questions(phrase)
     return render_template('search.html', questions=questions, phrase=phrase)
+
+@app.route('/question/<question_id>')
+def question_details(question_id):
+    answers= data_manager.get_question_with_answers(question_id)
+    return render_template('question.html', answers=answers, question_id=question_id)
+
+
+@app.route('/answer/<answer_id>/edit', methods=['GET', 'POST'])
+def edit_answer(answer_id):
+    if request.method== 'GET':
+        answer=data_manager.get_answer(answer_id)
+        return render_template('answer.html', answer=answer, answer_id=answer_id)
+    elif request.method == 'POST':
+        form_data = {
+            'answer_id': int(answer_id),
+            'answer_message': request.form['answer_message']
+        }
+        data_manager.update_answer(form_data)
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
--- data_manager.py
+++ data_manager.py
@@ -1,5 +1,6 @@
 import database_common
 
+
 @database_common.connection_handler
 def get_all_questions(cursor):
     cursor.execute("""
@@ -18,12 +19,11 @@ def get_all_answers(cursor):
     return all_answers
 
 
-
 @database_common.connection_handler
 def search_in_questions(cursor, phrase):
 
     query = """
-                    SELECT question.id, question.submission_time, question.view_number, question.vote_number, question.title, question.message, question.image 
+                    SELECT DISTINCT question.id AS ID, question.submission_time AS submission_time , question.view_number AS view_number, question.vote_number AS vote_number, question.title AS title, question.message AS message, question.image AS image, answer.id AS a_id, answer.question_id AS a_question_id, answer.submission_time AS a_submission_time, answer.vote_number AS a_vote_number, answer.message AS a_message, answer.image AS a_image 
                     FROM question
                     LEFT JOIN answer
                     ON question.id = answer.question_id
@@ -36,3 +36,51 @@ def search_in_questions(cursor, phrase):
     results=cursor.fetchall()
     return results
 
+
+@database_common.connection_handler
+def get_question_with_answers(cursor, question_id):
+    cursor.execute("""  
+                    SELECT question.id AS ID, question.submission_time AS submission_time , question.view_number AS view_number, question.vote_number AS vote_number, question.title AS title, question.message AS message, question.image AS image, answer.id AS a_id, answer.question_id AS a_question_id, answer.submission_time AS a_submission_time, answer.vote_number AS a_vote_number, answer.message AS a_message, answer.image AS a_image
+                    FROM question
+                    LEFT JOIN answer
+                    ON question.id=answer.question_id
+                    WHERE question.id=%(question_id)s;
+                    """,
+                   {'question_id': question_id})
+    question_with_answer = cursor.fetchall()
+    return question_with_answer
+
+
+@database_common.connection_handler
+def get_answer(cursor, answer_id):
+    cursor.execute("""
+                    SELECT * FROM answer
+                    WHERE answer.id=%(answer_id)s;
+                    """,
+                   {'answer_id': answer_id})
+    answer=cursor.fetchone()
+    return answer
+
+
+@database_common.connection_handler
+def update_answer(cursor, data):
+    cursor.execute("""
+                    UPDATE answer
+                    SET message=%s
+                    WHERE  id = %s ;
+                    """,
+                   (data['answer_message'],
+                    data['answer_id'])
+                   )
+
+
+@database_common.connection_handler
+def get_five_questions(cursor):
+    cursor.execute("""
+                    SELECT * FROM question
+                    ORDER BY submission_time DESC
+                    LIMIT 5;
+                   """)
+    five_questions = cursor.fetchall()
+    print(five_questions)
+    return five_questions
